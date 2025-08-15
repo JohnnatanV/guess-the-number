c06e9c35c17b8ab833829e24dcf68f4d2a551b41
JohnnatanV <joh.villaneda@outlook.com>
2025-08-14 21:00:59 -0500

    Comentarios codigo simple

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
-import './style.css'
+import "./style.css";
 
+function startGame(player: string, level: number) {
+  console.log(`Starting game for ${player} at level ${level}`);
+  // Game logic goes here
+}
 
+startGame("Player1", 1);
+// Additional game setup can be added here
