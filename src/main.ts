d1a0279f4a0c47090c331ee2b58f4f7b4518247c
JohnnatanV <joh.villaneda@outlook.com>
2025-08-18 01:40:34 -0500

    Basic practice for TS

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./style.css";
 
+const app = document.getElementById('app')
+
 function startGame(player: string, level: number) {
   console.log(`Starting game for ${player} at level ${level}`);
   // Game logic goes here
@@ -7,3 +9,30 @@ function startGame(player: string, level: number) {
 
 startGame("Player1", 1);
 // Additional game setup can be added here
+
+const menu = [
+  { name: "Margherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Veggie", price: 9 },
+]
+
+const cashInRegister = 100
+const orderQueue = []
+
+/**
+ * Challenge: Add a utility function "addNewPizza"
+ * that takes a pizza object and adds it to the menu
+ */
+
+const addNewPizzaOpt = (pizzaObj: { name: string, price: number }) => { menu.push(pizzaObj) }
+
+function addNewPizza(name: string, price: number) {
+  menu.push({ name, price })
+}
+
+addNewPizza("Double Chesse", 8)
+
+console.log(menu)
+
+
